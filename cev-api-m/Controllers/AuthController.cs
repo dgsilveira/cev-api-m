054f1b81f7bd8e6140691479445edb0cd06d466e
Douglas Silveira <dgsilveira@sga.pucminas.br>
2023-10-09 10:33:00 -0300

    Adicionar arquivos de projeto.

---
+++ cev-api-m/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using cev_api_m.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cev_api_m.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public IActionResult Auth(string username, string password)
+        {
+            var nomeUsuario = "admcev";
+            var senhaUsuario = "147852369";
+
+            if (username == nomeUsuario && password == senhaUsuario)
+            {
+                var token = TokenService.GenerateToken();
+                return Ok(token);
+            }
+
+            return BadRequest("usuário ou senha inválida");
+        }
+    }
+}
---
+++ cev-api-m/Controllers/VendedorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cev_api_m.Controllers
+{
+    [ApiController]
+    [Route("vendedor")]
+    public class VendedorController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok("oi");
+        }
+    }
+}
